1ada33b46a4ff94649466e66b993562381cc5b11
TorielLink <115531902+TorielLink@users.noreply.github.com>
2024-04-04 21:47:05 +0200

    On devait lancer deux intance d'application séparées et donc on ne pouvait communiquer aucune information entre les deux. Maintenant, tous les services sont en écoute à tout instants

--- src/app/Appli.java
+++ src/app/Appli.java
@@ -1,42 +1,26 @@
 package app;
 
 import serveur.Serveur;
-import serveur.Service;
 import serveur.ServiceEmpruntRetour;
 import serveur.ServiceReservation;
 
 import java.io.IOException;
-import java.sql.Time;
-import java.util.Timer;
 
 
 public class Appli {
+    private final static int PORT_RESERVATION = 3000;
+    private final static int PORT_EMPRUNT_RETOUR = 4000;
+
     public static void main(String[] args) {
-        Timer timer = new Timer(); // TODO : mettre le timer en commun
         new Thread(new Data()).start();
 
-        Class<? extends Service> service = null;
-        int port = -1;
-
-        if (args[0].equals("3000")) {
-            service = ServiceReservation.class;
-            port = 3000;
-        } else if (args[0].equals("4000")) {
-            service = ServiceEmpruntRetour.class;
-            port = 4000;
-        }
-
-        if (service != null) {
-            try {
-                new Thread(new Serveur(service, port)).start();
-                System.out.println("Serveur lance avec succes sur le port " + port);
-            } catch (IOException e) {
-                System.err.println("Probleme lors de la creation du serveur : " + e);
-            }
-        } else {
-            System.err.println("Probleme lors de la creation du serveur");
+        try {
+            new Thread(new Serveur(ServiceReservation.class, PORT_RESERVATION)).start();
+            System.out.println("Serveur lance avec succes sur le port " + PORT_RESERVATION);
+            new Thread(new Serveur(ServiceEmpruntRetour.class, PORT_EMPRUNT_RETOUR)).start();
+            System.out.println("Serveur lance avec succes sur le port " + PORT_EMPRUNT_RETOUR);
+        } catch (IOException e) {
+            System.err.println("Probleme lors de la creation du serveur : " + e);
         }
     }
-
-
 }
